bb568704c7e378b17b0b009a97252e163f171448
Abish-x-lethal <magarabish456@gmail.com>
2023-07-20 22:39:34 +0545

    bg update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
-export const page = () => {
+import Image from 'next/legacy/image'
+const page = () => {
   return (
-    <div>page</div>
-  )
+
+    <div className='relative h-screen'>
+    <Image
+    objectFit="cover"
+    layout="fill"
+    className='w-[100%] h-[100%] absoulte '
+    alt='momo 2d'
+    src="/bg.png"
+    />  
+
+    </div>
+    )
 }
+
+export default page
