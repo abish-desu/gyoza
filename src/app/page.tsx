2fb9bf92b802e170e5ed0499c77d6d243c7f9d19
Abish-x-lethal <magarabish456@gmail.com>
2023-07-22 23:02:30 +0545

    home-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import Image from 'next/legacy/image'
+import React from "react";
+import Image from "next/legacy/image";
 const page = () => {
   return (
-
-    <div className='relative h-screen'>
-    <Image
-   
-    layout="fill"
-    className='w-[100%] h-[100%] absoulte opacity-50 '
-    alt='momo'
-    src="/momo.jpeg"
-    />  
-
+    <div className="relative h-screen">
+      <Image
+        layout="fill"
+        className="absoulte opacity-50 "
+        alt="momo"
+        src="/momo.jpeg"
+      />
     </div>
-    )
-}
+  );
+};
 
-export default page
+export default page;
