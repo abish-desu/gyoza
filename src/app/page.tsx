a211cd4d43b58c74e7e3a87159a69efccdf944f1
Abish-x-lethal <magarabish456@gmail.com>
2023-07-22 23:34:04 +0545

    setup template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import Image from "next/legacy/image";
 const page = () => {
   return (
-    <div className="relative h-screen">
-      <Image
-        layout="fill"
-        className="absoulte opacity-50 "
-        alt="momo"
-        src="/momo.jpeg"
-      />
-    </div>
+ <div>
+<h1>HOME</h1>
+  </div>
   );
 };
 
