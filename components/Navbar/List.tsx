175ddbaf7bbd27823b8d9f5bcc03d653f3aa9c0c
Abish-x-lethal <magarabish456@gmail.com>
2023-08-01 21:34:45 +0545

    Form

--- components/Navbar/List.tsx
+++ components/Navbar/List.tsx
@@ -3,7 +3,7 @@ import { usePathname } from "next/navigation";
 
 import Link from "next/link";
 import React from "react";
-const isLoggedIn = false;
+
 
 const List = ({ isLoggedIn }:any) => {
   const navLinks = [
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import SignUp from '../../../components/SignUp/SignUp';
+import Login from '../../../components/Login/Login';
 
-const page = () => {
+const Page = () => {
+  const [activeLink, setActiveLink] = useState('signin');
+
+  const handleLinkClick = (link:any) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <section className="mt-10">
+      <div className="flex items-center justify-center">
+        <form className="w-full max-w-md">
+          <div className="flex items-center justify-center mt-6">
+            <button
+              type="button" 
+              className={`w-1/3 pb-4 font-medium text-center capitalize border-b-2 ${
+                activeLink === 'signin' ? 'border-blue-500 dark:border-blue-400' : 'dark:text-gray-300'
+              }`}
+              onClick={() => handleLinkClick('signin')}
+            >
+              sign in
+            </button>
+            <button
+              type="button" 
+              className={`w-1/3 pb-4 font-medium text-center capitalize border-b-2 ${
+                activeLink === 'signup' ? 'border-blue-500 dark:border-blue-400 dark:text-gray-300' : 'dark:border-gray-400 dark:text-gray-300'
+              }`}
+              onClick={() => handleLinkClick('signup')}
+            >
+              sign up
+            </button>
+          </div>
+          {activeLink === 'signup' ? <SignUp /> : <Login />}
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default Page;
