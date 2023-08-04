b3a3238f19fd035ab50e77eb548c7c013c8f14e0
Abish-x-lethal <magarabish456@gmail.com>
2023-08-04 17:14:03 +0545

    syntErr

--- components/SignUp/SignUp.tsx
+++ components/SignUp/SignUp.tsx
@@ -14,8 +14,8 @@ const SignUp = ({onSignInLinkClick}:any) => {
             stroke-width="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
             />
           </svg>
@@ -64,8 +64,8 @@ const SignUp = ({onSignInLinkClick}:any) => {
             stroke-width="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
             />
           </svg>
