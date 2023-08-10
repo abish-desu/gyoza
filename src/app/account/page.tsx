a0fd518f03b4ec42721b5fb543b1cf799ba9641a
Abish-x-lethal <magarabish456@gmail.com>
2023-08-10 21:17:45 +0545

    Login

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -4,33 +4,44 @@ import axios from "axios";
 import React, { useState } from "react";
 import SignUp from "../../../components/SignUp/SignUp";
 import Login from "../../../components/Login/Login";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   const [activeLink, setActiveLink] = useState("signin");
   const [isLoggedIn, setIsLoggedIn ]= useState<boolean>(false);
+  
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const router = useRouter()
   
 
   const handleSignSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/account', {name, email, password})
-    .then(res => {
-
-      if (res.data === "Success") {
-        alert("Registration successful!"); // Show an alert for success
-        setActiveLink("signin");
-      } else {
-        alert("Registration failed. Please try again.");
-      }
-
-    }).catch(err => console.log(err)) 
-   
+    axios.post('http://localhost:3001/account', { name, email, password, isLoggedInData: false }) // Set isLoggedInData to false for sign up
+      .then(res => {
+        if (res.data === "Success") {
+          alert("Registration successful!"); // Show an alert for success
+          setActiveLink("signin");
+        } else {
+          alert("Registration failed. Please try again.");
+        }
+      }).catch(err => console.log(err));
   };
+  
   const handleLogSubmit = (e: React.FormEvent) => {
-    e.preventDefault(); 
-   
+    e.preventDefault();
+    axios.post('http://localhost:3001/account', { name, password, isLoggedInData: true }) // Set isLoggedInData to true for login
+      .then(res => {
+        if (res.data.Status === "Success") {
+          setIsLoggedIn(true);// Set isLoggedIn state to true
+          alert("Login successful!"); // Show an alert for success
+          router.replace('/') 
+          // You can also handle any role-related logic here, e.g., set user role in state
+        } else {
+          alert("Login failed. Please check your credentials.");
+        }
+      }).catch(err => console.log(err));
   };
 
   const handleNameChange = (value: string) => {
@@ -57,6 +68,7 @@ const Page = () => {
   };
 
   return (
+    isLoggedIn? <h1>Hello You are logged</h1> :
     <section className="mt-10">
       <div className="flex items-center justify-center">
         <div className="w-full max-w-md">
