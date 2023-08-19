58ea552ba39a6b878fe08717201aff2733dded88
Abish-x-lethal <magarabish456@gmail.com>
2023-08-19 20:59:09 +0545

    dataUpdate

--- src/app/data.ts
+++ src/app/data.ts
@@ -9,22 +9,22 @@ img:string;
 
 const data =[
   {
-    id:1,name:"STEAM",price_1:150,price_2:170,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_steam.jpg"
+    id:1,name:"STEAM",price_1:170,price_2:150,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_steam.jpg"
   },
   {
-    id:2,name:"FRIED",price_1:150,price_2:200,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_fry.jpeg"
+    id:2,name:"FRIED",price_1:180,price_2:160,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_fry.jpeg"
   },
   {
-    id:3,name:"CHILLI",price_1:150,price_2:200,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_chilli.jpg"
+    id:3,name:"CHILLI",price_1:200,price_2:180,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_chilli.jpg"
   },
   {
-    id:4,name:"KOTHEY",price_1:150,price_2:200,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_kothey.jpg"
+    id:4,name:"KOTHEY",price_1:200,price_2:180,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_kothey.jpg"
   },
   {
-    id:5,name:"JHOL",price_1:150,price_2:200,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_jhol.png"
+    id:5,name:"JHOL",price_1:250,price_2:220,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_jhol.png"
   },
   {
-    id:6,name:"PLATTER",price_1:150,price_2:200,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_platter.jpeg"
+    id:6,name:"PLATTER",price_1:500,price_2:450,desc:"MOMO MERO SATHI MOMO MERO SATHI",img:"/momo_platter.jpeg"
   },
 ]
 export default data;
